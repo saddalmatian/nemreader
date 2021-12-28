68e20daa83931d3acea75994da618515a24b2877
saddalmatian <saddalmatian@gmail.com>
2021-12-28 16:14:23 +0700

    finish split and analyze file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
+import json
 import nem12_generator
+from nemreader import nem_objects
 from nemreader import read_nem_file
 
 # Prefix file when split into multiple files
@@ -48,28 +50,74 @@ def split_nem12_file(file_path: str, line_quantity: int):
                         nem_writer.writerow(record)
                 count = 0
                 number_id += 1
-                FILE_QUANTITY = number_id
+                FILE_QUANTITY = number_id-1
                 line_container = []
+
+
+def connect_readings(d1: dict, d2: dict):
+    """ Connect 2 readings dicts together, d2 connect to d1
+    :param d1: first dict
+    :param d2: second dict
+    """
+    for nmi in d2:
+        if nmi not in d1:
+            d1[nmi] = {}
+        for suffix in d2[nmi]:
+            if suffix not in d1[nmi]:
+                d1[nmi][suffix] = []
+            for readings in d2[nmi][suffix]:
+                d1[nmi].setdefault(suffix, []).append(readings)
+
+
+def connect_transaction(d1: dict, d2: dict):
+    """ Connect 2 transaction dicts together, d2 connect to d1
+    :param d1: first dict
+    :param d2: second dict
+    """
+    for nmi in d2:
+        if nmi not in d1:
+            d1[nmi] = {}
+        for suffix in d2[nmi]:
+            if suffix not in d1[nmi]:
+                d1[nmi][suffix] = []
+            for readings in d2[nmi][suffix]:
+                d1[nmi].setdefault(suffix, []).append([])
+
+
+def print_json(response):
+    print(json.dumps(response, sort_keys=True, indent=4, default=str))
 
 
 def main():
     # Generate nem12.csv
-    nem12_generator.write_to_csv("NEM12.csv", 10)
-    m = read_nem_file('NEM12.csv')
+    # nem12_generator.write_to_csv("NEM12.csv", 2)
 
-    # use library
+    # use library and analyse
+    # m = read_nem_file('NEM12.csv', 1)
+
     # for nmi in m.readings:
     #     for suffix in m.readings[nmi]:
-    #         for reading in m.readings[nmi][suffix][-1:]:
+    #         for reading in m.readings[nmi][suffix]:
     #             print(reading)
-    split_nem12_file('NEM12.csv', 500)
-    for file_id in range(1, FILE_QUANTITY):
+
+    # spilt file and analyse
+    split_nem12_file('NEM12.csv', 2)
+    for file_id in range(1, FILE_QUANTITY+1):
         if(file_id == 1):
-            m = read_nem_file(PREFIX_FILE+str(file_id) +
-                              ".csv", quantity=FILE_QUANTITY)
-    else:
-        m += read_nem_file(PREFIX_FILE+str(file_id)+".csv",
-                           ignore_missing_header=True)
+            m = read_nem_file(PREFIX_FILE+str(file_id) + ".csv",
+                              quantity=1)
+            nem = nem_objects.NEMFile(m.header, m.readings, m.transactions)
+        else:
+            m = read_nem_file(PREFIX_FILE+str(file_id) + ".csv",
+                              quantity=file_id,
+                              ignore_missing_header=True)
+            connect_readings(nem.readings, m.readings)
+            connect_transaction(nem.transactions, m.transactions)
+
+    for nmi in nem.readings:
+        for suffix in nem.readings[nmi]:
+            for reading in nem.readings[nmi][suffix]:
+                print(reading)
 
 
 if __name__ == "__main__":
